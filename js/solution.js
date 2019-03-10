c38956db3497864f423a60c2e693f0df746d58ee
Alexey <alex.erm@yandex.ru>
2019-03-10 15:58:25 +0300

    watch to ivan 2

--- js/solution.js
+++ js/solution.js
@@ -66,7 +66,7 @@ function setDefaults() {
     }
   }
 
-  if (!(localStorage.currentCoordinates)) {  
+  if (localStorage.currentCoordinates) {  
   menu.style.left = getCoordinatesMenu().x + 'px';
   menu.style.top = getCoordinatesMenu().y + 'px';
   }
@@ -750,5 +750,5 @@ function sendPngMask() {
 //   // }, 'image/png', 0.95);
 // }
 
-// menu.style.left = getCoordinatesMenu().x + 'px';
-// menu.style.top = getCoordinatesMenu().y + 'px';
+menu.style.left = getCoordinatesMenu().x + 'px';
+menu.style.top = getCoordinatesMenu().y + 'px';
